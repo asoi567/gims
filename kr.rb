e121dbff7cfff04b3c304c6eefd9475e649b037b
Mikhail Dieterle <mikdiet@gmail.com>
2017-12-23 12:48:30 +0300

    Custom image format

--- kr.rb
+++ kr.rb
@@ -17,34 +17,53 @@ Shoes.app height: 750, width: 1200 do
   @open_bmp_button.click do
     filename = ask_open_file
     if filename
-      @file = ProcessedFile.bmp(filename)
-      @save_bmp_button.state = nil
-      @save_custom_button.state = nil
-      @meta.text = @file.meta.inspect
-      @canvas.height = @file.height * 2
-      @canvas.width = @file.width * 2
-      @canvas.clear do
-        image do
-          @file.pixels.each_with_index do |row, x|
-            row.each_with_index do |cell, y|
-              stroke rgb(*cell)
-              rect x * 2 + 1, y * 2 + 1, 1, 1
-            end
-          end
-        end
-      end
+      @image = Image.bmp(filename)
+      draw_image
     end
   end
 
   @save_bmp_button.click do
     filename = ask_save_file
     if filename
-      @file.to_bmp("#{filename}.bmp")
+      @image.to_bmp("#{filename}.bmp")
+    end
+  end
+
+  @open_custom_button.click do
+    filename = ask_open_file
+    if filename
+      @image = Image.custom(filename)
+      draw_image
+    end
+  end
+
+  @save_custom_button.click do
+    filename = ask_save_file
+    if filename
+      @image.to_custom("#{filename}.bc")
+    end
+  end
+
+  def draw_image
+    @save_bmp_button.state = nil
+    @save_custom_button.state = nil
+    @meta.text = @image.meta.inspect
+    @canvas.height = @image.height * 2
+    @canvas.width = @image.width * 2
+    @canvas.clear do
+      image do
+        @image.pixels.each_with_index do |row, x|
+          row.each_with_index do |cell, y|
+            stroke rgb(*cell)
+            rect x * 2 + 1, y * 2 + 1, 1, 1
+          end
+        end
+      end
     end
   end
 end
 
-class ProcessedFile
+class Image
   # https://practicingruby.com/articles/binary-file-formats
   def self.bmp(filename)
     meta = {}
@@ -61,7 +80,7 @@ class ProcessedFile
       meta[:height].times do |x|
         pixels[x] = []
         meta[:width].times do
-          pixels[x] << [f.read(1).unpack("C").first, f.read(1).unpack("C").first, f.read(1).unpack("C").first].reverse
+          pixels[x] << f.read(3).unpack("C3").reverse
         end
         f.pos += meta[:width] % 4
       end
@@ -71,6 +90,26 @@ class ProcessedFile
   end
 
   def self.custom(filename)
+    meta = {}
+    pixels = []
+    File.open filename, 'rb' do |f|
+      header = f.read(12)
+      meta[:type], meta[:file_size], meta[:pixels_size], meta[:off_bits] = header.unpack("A2V2v")
+
+      header = f.read(25)
+      meta[:bits_per_pixel], meta[:width], meta[:author] = header.unpack("cVA20")
+
+      height = (meta[:pixels_size] * 3.0 / meta[:width]).floor
+
+      meta[:width].times do |x|
+        pixels[x] = []
+        height.times do
+          pixels[x] << f.read(3).unpack("C3")
+        end
+      end
+    end
+
+    new pixels, meta
   end
 
   attr_reader :pixels, :meta
@@ -82,22 +121,42 @@ class ProcessedFile
 
   def to_bmp(filename)
     File.open filename, 'wb' do |f|
-      f << ["BM", 54 + pixel_array_size, 0, 0, 54].pack("A2Vv2V")
-      f << [40, width, height, 1, 24, 0, pixel_array_size, 2835, 2835, 0, 0].pack("Vl<2v2V2l<2V2")
+      f << ["BM", 54 + bmp_pixel_array_size, 0, 0, 54].pack("A2Vv2V")
+      f << [40, width, height, 1, 24, 0, bmp_pixel_array_size, 2835, 2835, 0, 0].pack("Vl<2v2V2l<2V2")
       pixels.transpose.reverse.map do |row|
         row.map do |pixel|
-          f << pixel.reverse.map(&:chr).join
+          f << pixel.reverse.pack('c3')
         end
         f << "\x0" * (width % 4)
       end
     end
   end
 
-  def pixel_array_size
+  def bmp_pixel_array_size
     ((24 * width) / 32.0).ceil * 4 * height
   end
 
-  def to_custom
+  # 1. идентификатор типа файла (2 байта)
+  # 2. размер файла в байтах (4 байта)
+  # 4. размер растра в байтах (4 байта)
+  # 3. размер заголовка в байтах (2 байта)
+  # 9. глубина цвета (количество бит на один пиксель) (1 байт)
+  # 6. ширина изображения в пикселях (4 байта)
+  # 14. автор формата (20 байт)
+  def to_custom(filename)
+    File.open filename, 'wb' do |f|
+      f << ["BC", 37 + custom_pixel_array_size, custom_pixel_array_size, 37].pack("A2V2v")
+      f << [24, width, 'Mikhail Dieterle'].pack("cVA20")
+      pixels.map do |row|
+        row.map do |pixel|
+          f << pixel.pack('c3')
+        end
+      end
+    end
+  end
+
+  def custom_pixel_array_size
+    (width * height * 8 / 24.0).ceil
   end
 
   def height
